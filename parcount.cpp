a38aa3111be7628d7f4ada16cd109eb35d9806e4
Jiupeng Zhang <jiupeng.zhang@rochester.edu>
2019-02-23 15:12:08 -0500

    Added scaffold code

--- parcount.cpp
+++ parcount.cpp
@@ -5,28 +5,170 @@
 #include <atomic>
 #include <mutex>
 #include <chrono>
+#include <stdatomic.h>
+
+class lock;
+
+class cpp_mutex_lock;
+
+class tas_lock;
+
+class eback_tas_lock;
+
+class ticket_lock;
+
+class pback_ticket_lock;
+
+class mcs_lock;
+
+class k42_mcs_lock;
+
+class clh_lock;
+
+class k42_clh_lock;
 
 void set_affinity(pthread_t &thread, int i, int cores, int step);
 
 void run_tests(int t_cnt, int iter_cnt, int cores, int step);
 
-void test_cpp_mutex(int t_cnt, int iter_cnt, int cores, int step);
+void test(lock &lock, int t_cnt, int iter_cnt, int cores, int step);
 
-void test_naive_tas(int t_cnt, int iter_cnt, int cores, int step);
+class lock {
+public:
+    virtual void acquire() = 0;
 
-void test_eback_tas(int t_cnt, int iter_cnt, int cores, int step);
+    virtual void release() = 0;
+};
 
-void test_naive_ticket(int t_cnt, int iter_cnt, int cores, int step);
+// C++ mutex
+class cpp_mutex_lock : public lock {
+private:
+    std::mutex lock;
 
-void test_pback_ticket(int t_cnt, int iter_cnt, int cores, int step);
+public:
+    cpp_mutex_lock() = default;
 
-void test_mcs(int t_cnt, int iter_cnt, int cores, int step);
+    cpp_mutex_lock(cpp_mutex_lock const &other) {}
 
-void test_k42_mcs(int t_cnt, int iter_cnt, int cores, int step);
+    void acquire() override {
+        lock.lock();
+    }
 
-void test_clh(int t_cnt, int iter_cnt, int cores, int step);
+    void release() override {
+        lock.unlock();
+    }
+};
 
-void test_k42_clh(int t_cnt, int iter_cnt, int cores, int step);
+// naive TAS lock
+class tas_lock : public lock {
+private:
+    std::atomic_flag f = ATOMIC_FLAG_INIT;
+
+public:
+    tas_lock() = default;
+
+    tas_lock(tas_lock const &other) {}
+
+    void acquire() override {
+        while (f.test_and_set());
+    }
+
+    void release() override {
+        f.clear();
+    }
+};
+
+// TAS lock with well-tuned exponential backoff
+// you’ll need to experiment with different base, multiplier, and cap values
+// todo
+class eback_tas_lock : public lock {
+public:
+    eback_tas_lock() = default;
+
+    eback_tas_lock(eback_tas_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// naive ticket lock
+// todo
+class ticket_lock : public lock {
+public:
+    ticket_lock() = default;
+
+    ticket_lock(ticket_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// ticket lock with well-tuned proportional backoff
+// todo
+class pback_ticket_lock : public lock {
+public:
+    pback_ticket_lock() = default;
+
+    pback_ticket_lock(pback_ticket_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// MCS lock
+// todo
+class mcs_lock : public lock {
+public:
+    mcs_lock() = default;
+
+    mcs_lock(mcs_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// "K42" MCS lock with standard interface
+// todo
+class k42_mcs_lock : public lock {
+public:
+    k42_mcs_lock() = default;
+
+    k42_mcs_lock(k42_mcs_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// CLH lock
+// todo
+class clh_lock : public lock {
+public:
+    clh_lock() = default;
+
+    clh_lock(clh_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
+
+// “K42” CLH lock
+// todo
+class k42_clh_lock : public lock {
+public:
+    k42_clh_lock() = default;
+
+    k42_clh_lock(k42_clh_lock const &other) {}
+
+    void acquire() override {}
+
+    void release() override {}
+};
 
 
 int main(int argc, char *argv[]) {
@@ -67,7 +209,7 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void set_affinity(pthread_t &thread, int i, const int cores, const int step) {
+void set_affinity(pthread_t &thread, int __unused i, const int __unused cores, const int __unused step) {
 #ifdef __linux__
     if (cores == 0) return;
     cpu_set_t c_set;
@@ -82,49 +224,63 @@ void set_affinity(pthread_t &thread, int i, const int cores, const int step) {
 
 void run_tests(int t_cnt, int iter_cnt, const int cores, const int step) {
     std::cout << "running test_cpp_mutex..." << std::endl;
-    test_cpp_mutex(t_cnt, iter_cnt, cores, step);
+    auto cpp_mutex = cpp_mutex_lock();
+    test(cpp_mutex, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_naive_tas..." << std::endl;
-    test_naive_tas(t_cnt, iter_cnt, cores, step);
+    auto tas = tas_lock();
+    test(tas, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_eback_tas..." << std::endl;
-    test_eback_tas(t_cnt, iter_cnt, cores, step);
+    auto eback_tas = eback_tas_lock();
+    test(eback_tas, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_naive_ticket..." << std::endl;
-    test_naive_ticket(t_cnt, iter_cnt, cores, step);
+    auto ticket = ticket_lock();
+    test(ticket, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_pback_ticket..." << std::endl;
-    test_pback_ticket(t_cnt, iter_cnt, cores, step);
+    auto pback_ticket = pback_ticket_lock();
+    test(pback_ticket, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_mcs..." << std::endl;
-    test_mcs(t_cnt, iter_cnt, cores, step);
+    auto mcs = mcs_lock();
+    test(mcs, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_k42_mcs..." << std::endl;
-    test_k42_mcs(t_cnt, iter_cnt, cores, step);
+    auto k42_mcs = k42_mcs_lock();
+    test(k42_mcs, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_clh..." << std::endl;
-    test_clh(t_cnt, iter_cnt, cores, step);
+    auto clh = clh_lock();
+    test(clh, t_cnt, iter_cnt, cores, step);
+
     std::cout << "\nrunning test_k42_clh..." << std::endl;
-    test_k42_clh(t_cnt, iter_cnt, cores, step);
+    auto k42_clh = k42_clh_lock();
+    test(k42_clh, t_cnt, iter_cnt, cores, step);
 }
 
-// a C++ mutex
-void test_cpp_mutex(const int t_cnt, const int iter_cnt, const int cores, const int step) {
+void test(::lock &lock, const int t_cnt, const int iter_cnt, const int cores, const int step) {
     struct t_state {
-        std::mutex *lock;
+        ::lock *lock;
         std::atomic_bool *start;
         int *counter;
         int iter_cnt;
     };
     pthread_t threads[t_cnt];
     int counter = 0;
-    std::mutex lock;
     std::atomic_bool start;
     t_state params = {&lock, &start, &counter, iter_cnt};
     for (int i = 0; i < t_cnt; i++) {
         pthread_create(&(threads[i]), nullptr, [](void *args) -> void * {
             auto *state = static_cast<struct t_state *>(args);
-            std::mutex *l = state->lock;
             std::atomic_bool *trigger = state->start;
             int *cnt = state->counter, it_cnt = state->iter_cnt;
             while (!trigger->load());
             for (int t = 0; t < it_cnt; t++) {
-                l->lock();
+                state->lock->acquire();
                 *cnt = *cnt + 1;
-                l->unlock();
+                state->lock->release();
             }
             return nullptr;
         }, &params);
@@ -139,44 +295,4 @@ void test_cpp_mutex(const int t_cnt, const int iter_cnt, const int cores, const 
     std::chrono::duration<double, std::milli> diff = end_t - start_t;
     std::cout << "result of count: " << counter << std::endl;
     std::cout << "completed in " << diff.count() << " ms" << std::endl;
-}
-
-// a naive TAS lock
-void test_naive_tas(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a TAS lock with well-tuned exponential backoff (you’ll need to experiment with different base, multiplier, and cap values)
-void test_eback_tas(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a naive ticket lock
-void test_naive_ticket(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a ticket lock with well-tuned proportional backoff
-void test_pback_ticket(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// an MCS lock
-void test_mcs(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a "K42" MCS lock (with standard interface)
-void test_k42_mcs(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a CLH lock
-void test_clh(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
-}
-
-// a “K42” CLH lock
-void test_k42_clh(const int t_cnt, const int iter_cnt, const int cores, const int step) {
-    // todo
 }
